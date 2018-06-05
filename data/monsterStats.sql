3b73137976eb30626bcd168b8bae8bb5e6bdd7ee
TyBurns <mooflaghero@gmail.com>
2018-06-05 15:13:39 -0400

    Work on the monster data.

---
+++ data/monsterStats.sql
@@ -0,0 +1,14 @@
+create table monsterstats(
+    monname text primary key,
+    armorclass int,
+    hitdice text,
+    numofattacks text,
+    damage text,
+    movement text,
+    numappearing text,
+    saveas text,
+    morale text,
+    treasuretype text,
+    xp text,
+    descript text
+);
